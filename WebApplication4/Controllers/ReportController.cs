b096894351035a7e6b20ae2ba51d121782705189
Hisham(Home) <hisham.shahid@braincrop.net>
2023-08-21 00:30:34 +0500

    Error handling

--- WebApplication4/Controllers/ReportController.cs
+++ WebApplication4/Controllers/ReportController.cs
@@ -1,6 +1,8 @@
 ﻿using AspNetCore.Reporting;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using WebApiCoreWithEF.Context;
@@ -12,11 +14,13 @@ namespace WebApplication4.Controllers
     {
         private readonly IWebHostEnvironment _webHostEnvironment;
         private readonly CompanyContext _db;
-        public ReportController(IWebHostEnvironment webHostEnvironment, CompanyContext db)
+        private readonly ILogger _logger;
+        public ReportController(IWebHostEnvironment webHostEnvironment, CompanyContext db, ILogger<ReportController> logger)
         {
             this._webHostEnvironment = webHostEnvironment;
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
             _db = db;
+            _logger = logger;
         }
         public IActionResult Index()
         {
@@ -25,30 +29,60 @@ namespace WebApplication4.Controllers
 
         public IActionResult Print() 
         {
-            string mimetype = "";
-            int extension = 1;
-            var path = $"{this._webHostEnvironment.WebRootPath}\\Reports\\Report1.rdlc";
-            Dictionary<string, string> parameters = new Dictionary<string, string>();
-            parameters.Add("ReportParameter1", "Test Report");
-            LocalReport localReport = new LocalReport(path);
-            var result=localReport.Execute(RenderType.Pdf,extension,parameters,mimetype);
-            return File(result.MainStream, "application/pdf");
+            string error = "";
+            try
+            {
+                _logger.LogInformation("Log message in the Print() method");
+                string mimetype = "";
+                int extension = 1;
+                var path = $"{this._webHostEnvironment.WebRootPath}\\Reports\\Report1.rdlc";
+                Dictionary<string, string> parameters = new Dictionary<string, string>();
+                parameters.Add("ReportParameter1", "Test Report");
+                LocalReport localReport = new LocalReport(path);
+                var result = localReport.Execute(RenderType.Pdf, extension, parameters, mimetype);
+                return File(result.MainStream, "application/pdf");
+            }
+            catch (System.Exception Ex)
+            {
+
+                _logger.LogError(Ex.StackTrace.ToString());
+                ViewBag.MyString = Ex.StackTrace.ToString();
+                error = Ex.StackTrace.ToString();
+                //return View(Ex.StackTrace.ToString());
+            }
+            return Content(error);
         }
 
         public IActionResult MapData()
         {
-            var dt = new DataTable();
-            GetData getData = new GetData(_db);
-            dt = getData.Data();
-            string mimetype = "";
-            int extension = 1;
-            var path = $"{this._webHostEnvironment.WebRootPath}\\Reports\\Report2.rdlc";
-            Dictionary<string, string> parameters = new Dictionary<string, string>();
-            //parameters.Add("ReportParameter1", "Hello from hisham");
-            LocalReport localReport = new LocalReport(path);
-            localReport.AddDataSource("DataSet1", dt);
-            var result = localReport.Execute(RenderType.Pdf, extension, parameters, mimetype);
-            return File(result.MainStream, "application/pdf");
+            string error = "";
+            try
+            {
+                int i = Convert.ToInt32("abc");
+                _logger.LogInformation("Log message in the MapData() method");
+                var dt = new DataTable();
+                GetData getData = new GetData(_db);
+                dt = getData.Data();
+                string mimetype = "";
+                int extension = 1;
+                var path = $"{this._webHostEnvironment.WebRootPath}\\Reports\\Report2.rdlc";
+                Dictionary<string, string> parameters = new Dictionary<string, string>();
+                //parameters.Add("ReportParameter1", "Hello from hisham");
+                LocalReport localReport = new LocalReport(path);
+                localReport.AddDataSource("DataSet1", dt);
+                var result = localReport.Execute(RenderType.Pdf, extension, parameters, mimetype);
+                return File(result.MainStream, "application/pdf");
+            }
+            catch (System.Exception Ex)
+            {
+
+                _logger.LogError(Ex.StackTrace.ToString());
+                ViewBag.MyString = Ex.StackTrace.ToString();
+                error = Ex.StackTrace.ToString();
+                //return View(Ex.StackTrace.ToString());
+            }
+            return Content(error);
+          
         }
     }
 }
--- WebApplication4/Startup.cs
+++ WebApplication4/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,8 +31,9 @@ namespace WebApplication4
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddFile("Logs/mylog-{Date}.txt");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
