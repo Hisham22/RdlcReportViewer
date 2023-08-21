368b055729b311639fa128d021f6fe0e85c5b7a8
Hisham(Home) <hisham.shahid@braincrop.net>
2023-08-22 01:37:08 +0500

    test

--- WebApplication4/Controllers/ReportController.cs
+++ WebApplication4/Controllers/ReportController.cs
@@ -68,15 +68,19 @@ namespace WebApplication4.Controllers
                 var dt = new DataTable();
                 GetData getData = new GetData(_db);
                 dt = getData.Data();
-                string mimetype = "";
-                int extension = 1;
-                var path = $"{this._webHostEnvironment.WebRootPath}//Reports//Report2.rdlc";
-                Dictionary<string, string> parameters = new Dictionary<string, string>();
-                //parameters.Add("ReportParameter1", "Hello from hisham");
-                LocalReport localReport = new LocalReport(path);
-                localReport.AddDataSource("DataSet1", dt);
-                var result = localReport.Execute(RenderType.Pdf, extension, parameters, mimetype);
-                return File(result.MainStream, "application/pdf");
+                //string mimetype = "";
+                //int extension = 1;
+                //var path = $"{this._webHostEnvironment.WebRootPath}//Reports//Report2.rdlc";
+                //Dictionary<string, string> parameters = new Dictionary<string, string>();
+                ////parameters.Add("ReportParameter1", "Hello from hisham");
+                //LocalReport localReport = new LocalReport(path);
+                //localReport.AddDataSource("DataSet1", dt);
+                //var result = localReport.Execute(RenderType.Pdf, extension, parameters, mimetype);
+                //return File(result.MainStream, "application/pdf");
+                foreach (var item in dt)
+                {
+                    error += $"\n{item}";
+                }
             }
             catch (System.Exception Ex)
             {
